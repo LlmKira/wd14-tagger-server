d3d8a56755d747d1c7f27e913474838d94755152
Jasmine <coldlando@hotmail.com>
2024-07-29 17:05:37 +0800

    update model list

--- app/values.py
+++ app/values.py
@@ -6,11 +6,13 @@
 model_dir = "models"
 all_wd_models = (
     "wd-v1-4-moat-tagger-v2",
+    "wd-v1-4-swinv2-tagger-v2",
     "wd-v1-4-convnext-tagger-v2",
-    "wd-v1-4-convnext-tagger",
     "wd-v1-4-convnextv2-tagger-v2",
     "wd-v1-4-vit-tagger-v2",
     "wd-swinv2-tagger-v3",
     "wd-convnext-tagger-v3",
     "wd-vit-tagger-v3",
+    "wd-vit-large-tagger-v3",
+    "wd-eva02-large-tagger-v3"
 )
